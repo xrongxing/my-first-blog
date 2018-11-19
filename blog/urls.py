ac26f6914b686a1b52b1a9bf5cc4c0f2667c3399
Sam <Sam@example.com>
2018-11-19 18:07:56 +0800

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views as blog_views
 urlpatterns = [
     url(r'^$', blog_views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', blog_views.post_detail, name='post_detail'),
+    url(r'^post/new/$', blog_views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', blog_views.post_edit, name='post_edit'),
 ]
